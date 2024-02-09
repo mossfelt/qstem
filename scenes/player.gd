fea21c945f75958ae9724661190007556b11793d
Moss Felt <mossfelt@Mosss-MacBook-Pro.local>
2024-02-08 20:47:52 -0700

    added stuff

--- scenes/player.gd
+++ scenes/player.gd
@@ -14,6 +14,18 @@ func set_name_label(name):
 
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and _on_top():
 		selected = event.is_pressed()
-		print(selected)
+		
+func _on_top():
+	for dragable in get_tree().get_nodes_in_group("player_hovered_over"):
+		if (dragable.get_index() > get_index()):
+			return false
+	return true
+	
+func _on_area_2d_mouse_entered():
+	add_to_group("player_hovered_over")
+
+
+func _on_area_2d_mouse_exited():
+	remove_from_group("player_hovered_over")
